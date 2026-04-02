5f9d7fb2ca9fe6662de608c541a15311d5cc3748
AndreaClim <andrea.climaco@catolica.edu.sv>
2026-04-02 15:05:32 -0600

    Tabla para mostrar vehiculos (Controlador y Vista)

---
+++ Importames/Controllers/VehiculosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Importames.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("VehiculosView");
+        }
+    }
+}

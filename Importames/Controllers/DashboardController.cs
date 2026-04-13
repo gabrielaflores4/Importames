c5b27a492be8c93383dbe82c6d03c54121a80898
gabrielaflores4 <gabriela.flores9@catolica.edu.sv>
2026-04-13 14:43:04 -0600

    Estilos del Dashboard

--- Importames/Controllers/DashboardController.cs
+++ Importames/Controllers/DashboardController.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Importames.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Importames.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Dashboard()
         {
+            // Vehículos que NO están entregados
+            ViewBag.VehiculosPendientes = _context.Vehiculos
+                .Where(v => v.Estado.NombreEstado != "Entregado")
+                .Count();
+
+            // Vehículos entregados
+            ViewBag.Entregados = _context.Vehiculos
+                .Where(v => v.Estado.NombreEstado == "Entregado")
+                .Count();
+
+            // Total de clientes
+            ViewBag.Clientes = _context.Clientes.Count();
+
+            // Total de empleados
+            ViewBag.Empleados = _context.Usuarios.Count();
+
+            // Total inventario
+            ViewBag.TotalInventario = _context.Vehiculos
+                .Sum(v => v.Costo);
+
             return View();
         }
     }

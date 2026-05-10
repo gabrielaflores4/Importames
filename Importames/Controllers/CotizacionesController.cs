4705591a75027d26a45de5c24a0671f6684e468e
Amaya-2003 <caluchogamer@gmail.com>
2026-05-10 08:26:48 -0600

    Cotizaciones funcionales, Modulo terminado con sus operaciones algebraicas y aritmeticas. NO TOCAR (porfi pipipi)

--- Importames/Controllers/CotizacionesController.cs
+++ Importames/Controllers/CotizacionesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
+using SelectPdf;
+using System.Text.Json;
 
 namespace Importames.Controllers
 {
@@ -90,33 +92,435 @@ namespace Importames.Controllers
             });
         }
 
+        [HttpPost]
+        public IActionResult GenerarPDF([FromBody] JsonElement datos)
+        {
+            foreach (JsonProperty campo in datos.EnumerateObject())
+            {
+                if (string.IsNullOrWhiteSpace(campo.Value.ToString()))
+                {
+                    return BadRequest("Todos los campos deben estar llenos");
+                }
+            }
+
+            decimal ObtenerDecimal(string propiedad)
+            {
+                string valor = datos.GetProperty(propiedad).ToString();
+
+                if (string.IsNullOrWhiteSpace(valor))
+                    return 0;
+
+                return Convert.ToDecimal(valor, CultureInfo.InvariantCulture);
+            }
+
+            decimal precioFees = ObtenerDecimal("precioFees");
+            decimal comision = ObtenerDecimal("comision");
+            decimal transferencia = ObtenerDecimal("transferencia");
+            decimal storage = ObtenerDecimal("storage");
+            decimal tituloFedex = ObtenerDecimal("tituloFedex");
+            decimal grua = ObtenerDecimal("grua");
+            decimal depositoDeducible = ObtenerDecimal("depositoDeducible");
+            decimal bl = ObtenerDecimal("bl");
+            decimal flete = ObtenerDecimal("flete");
+            decimal comisionFlete = ObtenerDecimal("comisionFlete");
+
+            decimal fleteIBK = bl + flete + comisionFlete;
+
+            decimal impuesto = ObtenerDecimal("impuesto");
+            decimal tramiteAduanal = ObtenerDecimal("tramiteAduanal");
+            decimal gruaAduana = ObtenerDecimal("gruaAduana");
+            decimal venta = ObtenerDecimal("venta");
+            decimal pagoCuenta = ObtenerDecimal("pagoCuenta");
+
+            decimal reparacionPintor = ObtenerDecimal("reparacionPintor");
+            decimal repuestos = ObtenerDecimal("repuestos");
+            decimal tramitePlacas = ObtenerDecimal("tramitePlacas");
+            decimal autorizacion = ObtenerDecimal("autorizacion");
+            decimal emisionGases = ObtenerDecimal("emisionGases");
+            decimal experticie = ObtenerDecimal("experticie");
+            decimal experticieSistema = ObtenerDecimal("experticieSistema");
+
+            decimal total =
+                precioFees +
+                comision +
+                transferencia +
+                storage +
+                tituloFedex +
+                grua +
+                fleteIBK +
+                impuesto +
+                tramiteAduanal +
+                gruaAduana +
+                venta +
+                pagoCuenta +
+                reparacionPintor +
+                repuestos +
+                tramitePlacas +
+                autorizacion +
+                emisionGases +
+                experticie +
+                experticieSistema;
+
+            decimal subtotalDerecha =
+                precioFees +
+                comision +
+                transferencia +
+                storage +
+                grua +
+                tituloFedex;
+
+            decimal totalDerecha = subtotalDerecha - depositoDeducible;
+
+            string html = $@"
+<html>
+
+<head>
+
+<style>
+
+body {{
+    font-family: Arial;
+    padding:20px;
+}}
+
+table {{
+    border-collapse: collapse;
+    width: 100%;
+}}
+
+td, th {{
+    border: 1px solid black;
+    padding: 6px;
+    font-size: 12px;
+}}
+
+.titulo {{
+    text-align:center;
+    font-size:38px;
+    font-weight:bold;
+    margin-bottom:20px;
+}}
+
+.rojo {{
+    color:#c40000;
+    font-weight:bold;
+}}
+
+.amarillo {{
+    background:#fff3a1;
+}}
+
+.centrado {{
+    text-align:center;
+}}
+
+.derecha {{
+    text-align:right;
+}}
+
+.gris {{
+    background:#f2f2f2;
+}}
+
+</style>
+
+</head>
+
+<body>
+
+<div class='titulo'>
+    COTIZACION
+</div>
+
+<table>
+
+<tr>
+
+<td style='width:65%; vertical-align:top;'>
+
+<table>
+
+<tr>
+    <td colspan='2' class='rojo centrado'>
+        ESTIMADO DE VEHICULO
+    </td>
+</tr>
+
+<tr>
+    <td><b>NOMBRE CLIENTE</b></td>
+    <td>{datos.GetProperty("nombreCliente")}</td>
+</tr>
+
+<tr>
+    <td><b>TELEFONO</b></td>
+    <td>{datos.GetProperty("telefonoCliente")}</td>
+</tr>
+
+<tr>
+    <td><b>CORREO</b></td>
+    <td>{datos.GetProperty("correoCliente")}</td>
+</tr>
+
+<tr>
+    <td><b>MARCA VEHICULO</b></td>
+    <td>{datos.GetProperty("marcaVehiculo")}</td>
+</tr>
+
+<tr>
+    <td><b>FECHA SUBASTA</b></td>
+    <td>{datos.GetProperty("fechaSubasta")}</td>
+</tr>
+
+<tr>
+    <td><b>SUBASTA</b></td>
+    <td>{datos.GetProperty("subasta")}</td>
+</tr>
+
+<tr>
+    <td><b>NUMERO STOCK</b></td>
+    <td>{datos.GetProperty("numeroStock")}</td>
+</tr>
+
+<tr>
+    <td><b>LUGAR / ESTADO</b></td>
+    <td>{datos.GetProperty("lugarEstado")}</td>
+</tr>
+
+<tr>
+    <td><b>ADUANA DESTINO</b></td>
+    <td>{datos.GetProperty("aduanaDestino")}</td>
+</tr>
+
+<tr>
+    <td><b>COMPRA DE AUTO</b></td>
+
+    <td class='amarillo'>
+        $
+        {datos.GetProperty("apuestaVehiculo")}
+        +
+        FEES
+        $
+        {precioFees}
+    </td>
+</tr>
+
+<tr>
+    <td><b>COMISION</b></td>
+    <td>$ {comision}</td>
+</tr>
+
+<tr>
+    <td><b>TRANSFERENCIA</b></td>
+    <td>$ {transferencia}</td>
+</tr>
+
+<tr>
+    <td><b>STORAGE</b></td>
+    <td>$ {storage}</td>
+</tr>
+
+<tr>
+    <td><b>TITULO FEDEX</b></td>
+    <td>$ {tituloFedex}</td>
+</tr>
+
+<tr>
+    <td><b>GRUA USA</b></td>
+    <td>$ {grua}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>FLETE IBK</b></td>
+    <td>$ {fleteIBK}</td>
+</tr>
+
+<tr>
+    <td><b>IMPUESTO ADUANAL</b></td>
+    <td>$ {impuesto}</td>
+</tr>
+
+<tr>
+    <td><b>TRAMITE ADUANAL</b></td>
+    <td>$ {tramiteAduanal}</td>
+</tr>
+
+<tr>
+    <td><b>GRUA DE ADUANA</b></td>
+    <td>$ {gruaAduana}</td>
+</tr>
+
+<tr>
+    <td><b>VENTA</b></td>
+    <td>$ {venta}</td>
+</tr>
+
+<tr>
+    <td><b>PAGO A CUENTA</b></td>
+    <td>$ {pagoCuenta}</td>
+</tr>
+
+<tr>
+    <td><b>REPARACION PINTOR</b></td>
+    <td>$ {reparacionPintor}</td>
+</tr>
+
+<tr>
+    <td><b>REPUESTOS</b></td>
+    <td>$ {repuestos}</td>
+</tr>
+
+<tr>
+    <td><b>TRAMITE PLACAS</b></td>
+    <td>$ {tramitePlacas}</td>
+</tr>
+
+<tr>
+    <td><b>AUTORIZACION</b></td>
+    <td>$ {autorizacion}</td>
+</tr>
+
+<tr>
+    <td><b>EMISION GASES</b></td>
+    <td>$ {emisionGases}</td>
+</tr>
+
+<tr>
+    <td><b>EXPERTICIE</b></td>
+    <td>$ {experticie}</td>
+</tr>
+
+<tr>
+    <td><b>EXPERTICIE SISTEMA</b></td>
+    <td>$ {experticieSistema}</td>
+</tr>
+
+<tr>
+    <td colspan='2' class='amarillo derecha'>
+        <b>TOTAL: $ {total}</b>
+    </td>
+</tr>
+
+</table>
+
+</td>
+
+<td style='width:35%; vertical-align:top;'>
+
+<table>
+
+<tr>
+    <td class='rojo'><b>COMPRA VEHICULO</b></td>
+    <td>$ {precioFees}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>COMISION</b></td>
+    <td>$ {comision}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>TRANSFERENCIA</b></td>
+    <td>$ {transferencia}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>STORAGE</b></td>
+    <td>$ {storage}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>GRUA USA</b></td>
+    <td>$ {grua}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>TITULO FEDEX</b></td>
+    <td>$ {tituloFedex}</td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>SUB TOTAL</b></td>
+    <td class='amarillo'>
+        $ {subtotalDerecha}
+    </td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>DEPOSITO DEDUCIBLE</b></td>
+    <td>
+        $ {depositoDeducible}
+    </td>
+</tr>
+
+<tr>
+    <td class='rojo'><b>TOTAL</b></td>
+    <td class='amarillo'>
+        $ {totalDerecha}
+    </td>
+</tr>
+
+</table>
+
+</td>
+
+</tr>
+
+</table>
+
+<br><br>
+
+<div style='text-align:center; font-weight:bold; font-size:13px;'>
+
+Banco Agricola Cuenta # 003760364703
+<br>
+A nombre de: Carlos Roberto Amaya
+<br>
+Por favor anexar hojas verdes y derecho de deposito. Gracias!
+
+</div>
+
+</body>
+</html>";
+
+            HtmlToPdf converter = new HtmlToPdf();
+
+            PdfDocument doc = converter.ConvertHtmlString(html);
+
+            byte[] pdf = doc.Save();
+
+            doc.Close();
+
+            return File(pdf, "application/pdf", "Cotizacion.pdf");
+        }
 
         [HttpPost]
         public JsonResult ValidarCampos(List<string> campos)
         {
-
             foreach (var campo in campos)
             {
-
-                if (string.IsNullOrWhiteSpace(campo))
+                if (campo == null)
                 {
-
                     return Json(new
                     {
                         success = false,
                         message = "Todos los campos deben estar llenos"
                     });
-
                 }
 
+                if (string.IsNullOrWhiteSpace(campo.Trim()))
+                {
+                    return Json(new
+                    {
+                        success = false,
+                        message = "Todos los campos deben estar llenos"
+                    });
+                }
             }
 
             return Json(new
             {
                 success = true
             });
-
         }
-
     }
 }
--- Importames/wwwroot/js/cotizaciones.js
+++ Importames/wwwroot/js/cotizaciones.js
@@ -1,75 +1,216 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
 
-    document.getElementById("apuestaVehiculo")
-        .addEventListener("keyup", function () {
+    const apuestaVehiculo = document.getElementById("apuestaVehiculo");
 
-            let apuesta = this.value;
+    apuestaVehiculo.addEventListener("keyup", function () {
 
-            if (apuesta === "") {
+        let apuesta = this.value;
 
-                document.getElementById("precioFees").value = "";
-                return;
-            }
+        if (apuesta === "") {
 
-            fetch('/Cotizaciones/CalcularPrecioConFees', {
+            document.getElementById("precioFees").value = "";
+            return;
+        }
 
-                method: 'POST',
+        fetch('/Cotizaciones/CalcularPrecioConFees', {
 
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
+            method: 'POST',
 
-                body: 'apuesta=' + apuesta
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
 
-            })
-                .then(response => response.json())
-                .then(data => {
+            body: 'apuesta=' + apuesta
 
-                    document.getElementById("precioFees").value =
-                        data.precioConFees;
+        })
+            .then(response => response.json())
+            .then(data => {
 
-                });
+                document.getElementById("precioFees").value =
+                    data.precioConFees;
 
-        });
+            });
 
-    document.getElementById("btnListo")
-        .addEventListener("click", function () {
-
-            let formData = new URLSearchParams();
-
-            document.querySelectorAll("input, select")
-                .forEach(function (campo, index) {
-
-                    formData.append("campos", campo.value);
-
-                });
-
-            fetch('/Cotizaciones/ValidarCampos', {
-
-                method: 'POST',
-
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-
-                body: formData
-
-            })
-                .then(response => response.json())
-                .then(data => {
-
-                    if (!data.success) {
-
-                        alert(data.message);
-                        return;
-                    }
-
-                    alert("Campos completos");
-
-                    // AQUI DESPUES GENERARAS EL PDF
-
-                });
-
-        });
+    });
 
 });
+
+function generarPDF() {
+
+    let campos = document.querySelectorAll("input, select");
+
+    for (let campo of campos) {
+
+        if (campo.type === "hidden") {
+            continue;
+        }
+
+        if (campo.value == null) {
+            continue;
+        }
+
+        let valor = campo.value.trim();
+
+        if (valor === "") {
+
+            let nombreCampo = campo.id || "sin nombre";
+
+            alert("El campo está vacío: " + nombreCampo);
+
+            campo.focus();
+
+            return;
+        }
+    }
+
+    let datos = {
+
+        depositoDeducible:
+            document.getElementById("depositoDeducible").value,
+
+        apuestaVehiculo:
+            document.getElementById("apuestaVehiculo").value,
+
+        precioFees:
+            document.getElementById("precioFees").value,
+
+        comision:
+            document.getElementById("comision").value,
+
+        grua:
+            document.getElementById("grua").value,
+
+        bl:
+            document.getElementById("bl").value,
+
+        flete:
+            document.getElementById("flete").value,
+
+        comisionFlete:
+            document.getElementById("comisionFlete").value,
+
+        impuesto:
+            document.getElementById("impuesto").value,
+
+        tramiteAduanal:
+            document.getElementById("tramiteAduanal").value,
+
+        gruaAduana:
+            document.getElementById("gruaAduana").value,
+
+        transferencia:
+            document.getElementById("transferencia").value,
+
+        storage:
+            document.getElementById("storage").value,
+
+        tituloFedex:
+            document.getElementById("tituloFedex").value,
+
+        venta:
+            document.getElementById("venta").value,
+
+        pagoCuenta:
+            document.getElementById("pagoCuenta").value,
+
+        reparacionPintor:
+            document.getElementById("reparacionPintor").value,
+
+        repuestos:
+            document.getElementById("repuestos").value,
+
+        tramitePlacas:
+            document.getElementById("tramitePlacas").value,
+
+        autorizacion:
+            document.getElementById("autorizacion").value,
+
+        emisionGases:
+            document.getElementById("emisionGases").value,
+
+        experticie:
+            document.getElementById("experticie").value,
+
+        experticieSistema:
+            document.getElementById("experticieSistema").value,
+
+        nombreCliente:
+            document.getElementById("nombreCliente").value,
+
+        telefonoCliente:
+            document.getElementById("telefonoCliente").value,
+
+        correoCliente:
+            document.getElementById("correoCliente").value,
+
+        marcaVehiculo:
+            document.getElementById("marcaVehiculo").value,
+
+        fechaSubasta:
+            document.getElementById("fechaSubasta").value,
+
+        subasta:
+            document.getElementById("subasta").value,
+
+        numeroStock:
+            document.getElementById("numeroStock").value,
+
+        lugarEstado:
+            document.getElementById("lugarEstado").value,
+
+        aduanaDestino:
+            document.getElementById("aduanaDestino").value,
+
+        naviera:
+            document.getElementById("naviera").value
+    };
+
+    fetch('/Cotizaciones/GenerarPDF', {
+
+        method: 'POST',
+
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify(datos)
+
+    })
+        .then(response => {
+
+            if (!response.ok) {
+
+                return response.text().then(text => {
+                    throw new Error(text);
+                });
+            }
+
+            return response.blob();
+
+        })
+        .then(blob => {
+
+            const url = window.URL.createObjectURL(blob);
+
+            const a = document.createElement("a");
+
+            a.href = url;
+
+            a.download = "Cotizacion.pdf";
+
+            document.body.appendChild(a);
+
+            a.click();
+
+            a.remove();
+
+            window.URL.revokeObjectURL(url);
+
+        })
+        .catch(error => {
+
+            alert(error.message);
+
+        });
+
+}

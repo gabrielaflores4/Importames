5887ea017b7d2ab69a16dd97542ec5b77e9c6cb2
gabrielaflores4 <gabriela.flores9@catolica.edu.sv>
2026-05-12 10:27:01 -0600

    Corrección de Colores de la Interfaz

---
+++ Importames/wwwroot/js/sidebar.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const toggleBtn = document.getElementById("toggleSidebar");
+    const sidebar = document.querySelector(".sidebar");
+    const mainContent = document.querySelector(".main-content");
+    const topbar = document.querySelector(".topbar");
+
+    toggleBtn.addEventListener("click", function (e) {
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        sidebar.classList.toggle("closed");
+        mainContent.classList.toggle("expanded");
+        topbar.classList.toggle("expanded");
+
+    });
+
+});
